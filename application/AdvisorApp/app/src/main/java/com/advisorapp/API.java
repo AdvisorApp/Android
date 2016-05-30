c8f0ab81aca123006232b0409a105b83c593f595
SinigagliaSteeve <steeve.sinigaglia@laposte.net>
2016-05-30 15:36:05 +0200

    [ADV-41] build layout for register

--- application/AdvisorApp/app/src/main/java/com/advisorapp/API.java
+++ application/AdvisorApp/app/src/main/java/com/advisorapp/API.java
@@ -4,7 +4,8 @@ package com.advisorapp;
  * Created by Steeve on 29/05/2016.
  */
 public interface API {
-    String URL = "http://192.168.0.13:8090/api/";
+    String IP = "192.168.43.216";
+    String URL = "http://"+IP+":8090/api/";
     String AUTHENT_ROUTE = URL+"auths/token";
-    String USERS = URL+"users/";
+    String USERS = URL+"users";
 }
--- application/AdvisorApp/app/src/main/java/com/advisorapp/view/register/SignupActivity.java
+++ application/AdvisorApp/app/src/main/java/com/advisorapp/view/register/SignupActivity.java
@@ -10,19 +10,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.advisorapp.API;
 import com.advisorapp.AdvisorAppApplication;
 import com.advisorapp.R;
+import com.advisorapp.model.User;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 
+import org.codehaus.jackson.map.ObjectMapper;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -163,47 +171,15 @@ public class SignupActivity extends AppCompatActivity {
         @Override
         protected Boolean doInBackground(String... params) {
 
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, API.USERS, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    Toast.makeText(SignupActivity.this, response, Toast.LENGTH_LONG).show();
-                    //TODO la response est le token de l'user, à implémenter + récupérer les données
-                }
-            },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(SignupActivity.this, error.toString(), Toast.LENGTH_LONG).show();
-                        }
+            final User user = new User();
+            user.setEmail("test@gmail.com");
+            user.setFirstName("firstNameTest");
+            user.setLastName("lastNameTest");
+            user.setPassword("testPWD");
 
-                    }) {
-                @Override
-                protected Map<String, String> getParams() {
-                    Map<String, String> params = new HashMap<>();
-                    params.put("first_name", "firsttest");
-                    params.put("last_name", "lasttest");
-                    params.put("remote_id", "idtest");
-                    params.put("email", "azerty@gamil.com");
-                    params.put("password", "iasass");
-                    return params;
-                }
-            };
+//            postUserV1(user);
 
-            try {
-                Log.d(TAG,stringRequest.getHeaders().toString());
-            } catch (AuthFailureError authFailureError) {
-                authFailureError.printStackTrace();
-            }
 
-            try {
-                Map<String,String> map = stringRequest.getHeaders();
-                for (String key : map.keySet()) {
-                    Log.d(TAG,key);
-                }
-            } catch (AuthFailureError authFailureError) {
-                authFailureError.printStackTrace();
-            }
-            mRequestQueue.add(stringRequest);
             return true; //todo manage en fonction token
         }
 
@@ -212,4 +188,83 @@ public class SignupActivity extends AppCompatActivity {
             progressDialog.dismiss();
         }
     }
+//
+//    private void postUserV1(final User user){
+//
+//        StringRequest postRequest = new StringRequest(Request.Method.POST, API.USERS,
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//                        // response
+//                        Log.d("Response", response);
+//                    }
+//                },
+//                new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        // error
+//                        Log.d("Error.Response", error.toString());
+//                    }
+//                }
+//        ) {
+//            @Override
+//            protected Map<String, String> getParams() {
+//                Map<String, String> params = new HashMap<String, String>();
+//                params.put("firstname", "testfirstname");
+//                params.put("lastname", "testlastname");
+//                params.put("email", "testpwd");
+//                return params;
+//            }
+//
+//            @Override
+//            public String getBodyContentType() {
+//                return "application/json; charset=UTF-8";
+//            }
+//
+//            @Override
+//            public byte[] getBody() throws AuthFailureError {
+//                ObjectMapper mapper = new ObjectMapper();
+//                try {
+//                    return mapper.writeValueAsString(user).getBytes();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                return null;
+//            }
+//        };
+//        mRequestQueue.add(postRequest);
+//    }
+//
+//    private void postUserV2(User user){
+//        ObjectMapper mapper = new ObjectMapper();
+//        try {
+//            String jsonUser = mapper.writeValueAsString(user);
+//            JsonObjectRequest myRequest = new JsonObjectRequest(Request.Method.POST,
+//                    API.USERS, new JSONObject(jsonUser), new Response.Listener<JSONObject>() {
+//                @Override
+//                public void onResponse(JSONObject response) {
+//                    Log.d("Response", response.toString());
+//                }
+//            },
+//                    new Response.ErrorListener() {
+//                        @Override
+//                        public void onErrorResponse(VolleyError error) {
+//                            Log.d("Error.Response", error.toString());
+//                        }
+//                    }){
+//                @Override
+//                public Map<String, String> getHeaders() throws AuthFailureError {
+//                    HashMap<String, String> headers = new HashMap<String, String>();
+//                    headers.put("Content-Type", "application/json; charset=utf-8");
+//                    return headers;
+//                }
+//            };
+//            mRequestQueue.add(myRequest);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
 }
