0109c2141ebe711a7ca16450853e79039be8a0b3
SinigagliaSteeve <steeve.sinigaglia@laposte.net>
2016-06-10 13:45:15 +0200

    [ADV-35] refract view remaining uv

--- application/AdvisorApp/app/src/main/java/com/advisorapp/api/API.java
+++ application/AdvisorApp/app/src/main/java/com/advisorapp/api/API.java
@@ -5,7 +5,7 @@ package com.advisorapp.api;
  */
 public interface API {
 
-    String IP = "192.168.42.213";
+    String IP = "192.168.43.216";
     String URL = "http://"+IP+":8090/api/";
     String AUTHENT_ROUTE = URL+"auths/token";
     String USERS_ME_ROUTE = URL+"users/me";
