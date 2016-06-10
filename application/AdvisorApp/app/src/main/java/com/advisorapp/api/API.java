7a7963ed06c70517d0d004bb8fba94ceb769ec7f
SinigagliaSteeve <steeve.sinigaglia@laposte.net>
2016-06-10 12:24:26 +0200

    [ADV-35] fix bug on listner

--- application/AdvisorApp/app/src/main/java/com/advisorapp/api/API.java
+++ application/AdvisorApp/app/src/main/java/com/advisorapp/api/API.java
@@ -4,7 +4,7 @@ package com.advisorapp.api;
  * Created by Steeve on 29/05/2016.
  */
 public interface API {
-
+    
     String IP = "192.168.42.213";
     String URL = "http://"+IP+":8090/api/";
     String AUTHENT_ROUTE = URL+"auths/token";
